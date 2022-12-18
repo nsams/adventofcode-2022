3aee85bc80455bcfac3cdb0292311d77c0af1e63
Niko Sams <ns@vivid-planet.com>
2022-12-18 17:17:49 +0100

    17

---
+++ 17/run.ts
@@ -0,0 +1,179 @@
+import { readFileSync } from "fs";
+
+//xx:xx - xx:xx first part
+//xx:xx - xx:xx second part
+
+const jets = readFileSync("input.txt").toString().split("");
+//const jets = readFileSync("test-input.txt").toString().split("");
+
+class Shape {
+    public map: boolean[][] = [];
+    constructor(map: boolean[][]) {
+        for (let y = 0; y < map.length; y++) {
+            for (let x = 0; x < map[y].length; x++) {
+                if (!this.map[x]) this.map[x] = [];
+                this.map[x][y] = map[y][x];
+            }
+        }
+    }
+    get height() {
+        return this.map[0].length;
+    }
+    get width() {
+        return this.map.length;
+    }
+}
+class Chamber {
+    public map: Record<number, Record<number, true>> = {};
+    public height = 0;
+    constructor() {}
+
+    merge(shape: Shape, atX: number, atY: number) {
+        for (let x = 0; x < shape.map.length; x++) {
+            for (let y = 0; y < shape.map[x].length; y++) {
+                if (shape.map[x][y]) {
+                    if (!this.map[x + atX]) this.map[x + atX] = {};
+                    this.map[x + atX][atY - y] = true;
+                }
+            }
+        }
+        this.height = Math.max(this.height, atY);
+    }
+    intersects(shape: Shape, atX: number, atY: number) {
+        for (let x = 0; x < shape.map.length; x++) {
+            for (let y = 0; y < shape.map[x].length; y++) {
+                if (shape.map[x][y] && this.map[x + atX] && this.map[x + atX][atY - y]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    print(shape?: { shape: Shape; atX: number; atY: number }) {
+        const height = Math.max(this.height, shape?.atY || 0);
+
+        for (let y = height; y > 0; y--) {
+            process.stdout.write(y + "|");
+            for (let x = 0; x < 7; x++) {
+                //console.log(x, y, x - atX, atY - y);
+                if (this.map[x] && this.map[x][y]) {
+                    process.stdout.write("#");
+                } else if (shape && shape.shape.map[x - shape.atX] && shape.shape.map[x - shape.atX][shape.atY - y]) {
+                    process.stdout.write("@");
+                } else {
+                    process.stdout.write(".");
+                }
+            }
+            process.stdout.write("|\n");
+        }
+        process.stdout.write("+-------+\n\n");
+    }
+}
+const shapes = [
+    new Shape([[true, true, true, true]]),
+    new Shape([
+        [false, true, false],
+        [true, true, true],
+        [false, true, false],
+    ]),
+    new Shape([
+        [false, false, true],
+        [false, false, true],
+        [true, true, true],
+    ]),
+    new Shape([[true], [true], [true], [true]]),
+    new Shape([
+        [true, true],
+        [true, true],
+    ]),
+];
+
+const debugRockNumber = 0;
+
+let rocksStopped = 0;
+let nextShapeNum = 0;
+let nextJetNum = 0;
+const chamber = new Chamber();
+while (true) {
+    const shape = shapes[nextShapeNum];
+    nextShapeNum++;
+    if (nextShapeNum >= shapes.length) nextShapeNum = 0;
+
+    let shapeX = 2;
+    let shapeY = chamber.height + 3 + shape.height;
+
+    if (rocksStopped == debugRockNumber) chamber.print({ shape, atX: shapeX, atY: shapeY });
+
+    while (true) {
+        //move left/right
+        {
+            let shapeXMoveOffset = 0;
+            const jetDirection = jets[nextJetNum];
+            nextJetNum++;
+            if (nextJetNum >= jets.length) {
+                nextJetNum = 0;
+                console.log("wrap jets");
+            }
+            if (jetDirection == ">") {
+                if (shapeX < 7 - shape.width) {
+                    shapeXMoveOffset = +1;
+                }
+            } else if (jetDirection == "<") {
+                if (shapeX > 0) {
+                    shapeXMoveOffset = -1;
+                }
+            }
+
+            if (!chamber.intersects(shape, shapeX + shapeXMoveOffset, shapeY)) {
+                shapeX += shapeXMoveOffset;
+                console.log("move", nextJetNum - 1, jetDirection);
+                //chamber.print({ shape, atX: shapeX, atY: shapeY });
+                if (rocksStopped == debugRockNumber) chamber.print({ shape, atX: shapeX, atY: shapeY });
+            } else {
+                console.log("don't move", nextJetNum - 1, jetDirection);
+            }
+        }
+
+        //is blocked?
+        {
+            if (shapeY == shape.height) {
+                console.log("floor reached", shapeY, shape.height);
+                chamber.merge(shape, shapeX, shapeY);
+                if (rocksStopped == debugRockNumber) chamber.print();
+                rocksStopped++;
+
+                break;
+            }
+            if (chamber.intersects(shape, shapeX, shapeY - 1)) {
+                console.log("block reached (down)");
+                chamber.merge(shape, shapeX, shapeY);
+                if (rocksStopped == debugRockNumber) chamber.print();
+                rocksStopped++;
+                break;
+            }
+        }
+
+        //fall down
+        console.log("fall down", shapeY, shape.height);
+        shapeY--;
+        if (rocksStopped == debugRockNumber) chamber.print({ shape, atX: shapeX, atY: shapeY });
+    }
+    if (rocksStopped >= 2022) break;
+    //if (rocksStopped >= 6) break;
+}
+chamber.print();
+console.log(rocksStopped);
+console.log(chamber.height);
+/*
+1
+-1
+-1
+-1
+1
+1
+1
+-1
+-1
+1
+*/
